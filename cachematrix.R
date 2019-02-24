b4a7e69559cb7805f56c359f6f0d6d3690d5a3dc
Sougata Bhattacharya <sougatabhattacharya@Sougatas-MacBook-Pro.local>
2019-02-24 08:06:53 +0530

    Fixed the assignments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Overall function description makeCacheMatrix
+## This function sets a matrix (x), gets its value  and  
+## then sets the value of the inverse of x and gets its value.
+## It caches the value. 
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
@@ -16,7 +16,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function firstly checks whether the inverse of x has 
+## been computed. If so, it gets it from cache and skips computation. 
+## Otherwise, it calculates the inverse of x (using Solve()) 
+## and sets the value of the inverse in the cache via the setinverse function.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
